4bafb74b194fd7518b611e7eb1f3c95ffda67f7a
martin-mcinerney <martin_mcinerney@live.ie>
2022-11-10 19:27:25 +0000

    Adds admin for category and product

--- products/admin.py
+++ products/admin.py
@@ -2,6 +2,26 @@ from django.contrib import admin
 from .models import Product, Category
 # Register your models here.
 
-admin.site.register(Product)
-admin.site.register(Category)
+
+class ProductAdmin(admin.ModelAdmin):
+    list_display = (
+        'sku', 
+        'name', 
+        'category',
+        'price',
+        'rating',
+        'image',
+    )
+    ordering = ('name',)
+
+    
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = (
+        'name', 
+        'friendly_name',
+    )
+
+admin.site.register(Product, ProductAdmin)
+
+admin.site.register(Category, CategoryAdmin)
 
--- products/models.py
+++ products/models.py
@@ -3,6 +3,9 @@ from django.db import models
 # Create your models here.
 
 class Category(models.Model):
+
+    verbose_name_plural = 'Categories'
+    
     name = models.CharField(max_length = 254)
     friendly_name = models.CharField(max_length = 254, null = True, blank = True)
 

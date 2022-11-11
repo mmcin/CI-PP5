15b50cf98003e030071ab62cee581daad3adfe1d
martin-mcinerney <martin_mcinerney@live.ie>
2022-11-11 12:09:38 +0000

    Adds category search filters to products

--- products/views.py
+++ products/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render, get_object_or_404, redirect, reverse
 from django.db.models import Q
-from .models import Product 
+from .models import Product, Category
 
 
 
@@ -9,8 +9,17 @@ def all_products(request):
     """A view to show all products, including sorting and search queries"""
     products = Product.objects.all()
     query = None
+    categories = None
+
+
 
     if request.GET:
+
+        if 'category' in request.GET:
+            categories = request.GET['category'].split(',')
+            products = products.filter(category__name__in=categories)
+            categories = Category.objects.filter(name__in=categories)
+
         if 'q' in request.GET:
             query = request.GET['q']
             if not query:
@@ -24,6 +33,7 @@ def all_products(request):
     context = {
         'products':products,
         'search_term': query,
+        'current_categories': categories,
     }
     return render(request, 'products/products.html', context)
 

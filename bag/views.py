88ff01b5392aa7adc8577fe9ac49ece9359783fd
martin-mcinerney <martin_mcinerney@live.ie>
2022-11-13 20:23:20 +0000

    Added toast to edit quantity bag

--- bag/views.py
+++ bag/views.py
@@ -46,6 +46,7 @@ def adjust_bag(request, item_id):
     else:
         if quantity > 0:
             bag[item_id] = quantity
+            messages.success(request, f'Updated {product.name} quantity to {bag[item_id]}')
         else:
             bag.pop(item_id)
             messages.success(request, f'Removed {product.name} from your bag')

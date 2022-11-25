574daf2d4d3aaad6000423d0b84ed0d372548a3f
martin-mcinerney <martin_mcinerney@live.ie>
2022-11-25 19:53:40 +0000

    Adds product admin form

--- products/views.py
+++ products/views.py
@@ -1,7 +1,10 @@
-from django.shortcuts import render, get_object_or_404, redirect, reverse
+from django.shortcuts import render, redirect, reverse, get_object_or_404
+from django.contrib import messages
 from django.db.models import Q
-from .models import Product, Category
 from django.db.models.functions import Lower
+
+from .models import Product, Category
+from .forms import ProductForm
 
 
 
@@ -64,3 +67,13 @@ def product_detail(request, product_id):
         'product':product,
     }
     return render(request, 'products/product_detail.html', context)
+
+def add_product(request):
+    """ Add a product to the store """
+    form = ProductForm()
+    template = 'products/add_product.html'
+    context = {
+        'form': form,
+    }
+
+    return render(request, template, context)

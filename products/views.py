05a992e2d3f330b816efe37e06a33a8a7e6cc626
martin-mcinerney <martin_mcinerney@live.ie>
2022-11-25 20:15:16 +0000

    Adds the edit product page and view

--- products/views.py
+++ products/views.py
@@ -6,18 +6,16 @@ from django.db.models.functions import Lower
 from .models import Product, Category
 from .forms import ProductForm
 
-
-
 # Create your views here.
+
 def all_products(request):
-    """A view to show all products, including sorting and search queries"""
+    """ A view to show all products, including sorting and search queries """
+
     products = Product.objects.all()
     query = None
     categories = None
     sort = None
     direction = None
-
-    
 
     if request.GET:
         if 'sort' in request.GET:
@@ -28,13 +26,12 @@ def all_products(request):
                 products = products.annotate(lower_name=Lower('name'))
             if sortkey == 'category':
                 sortkey = 'category__name'
-            
             if 'direction' in request.GET:
                 direction = request.GET['direction']
                 if direction == 'desc':
                     sortkey = f'-{sortkey}'
             products = products.order_by(sortkey)
-
+            
         if 'category' in request.GET:
             categories = request.GET['category'].split(',')
             products = products.filter(category__name__in=categories)
@@ -43,30 +40,35 @@ def all_products(request):
         if 'q' in request.GET:
             query = request.GET['q']
             if not query:
-                messages.error(request, "You didn't enter any search criteria")
+                messages.error(request, "You didn't enter any search criteria!")
                 return redirect(reverse('products'))
+            
             queries = Q(name__icontains=query) | Q(description__icontains=query)
             products = products.filter(queries)
 
     current_sorting = f'{sort}_{direction}'
 
     context = {
-        'products':products,
+        'products': products,
         'search_term': query,
         'current_categories': categories,
         'current_sorting': current_sorting,
     }
+
     return render(request, 'products/products.html', context)
 
+
 def product_detail(request, product_id):
-    """A view to show individual product detail"""
-    
+    """ A view to show individual product details """
+
     product = get_object_or_404(Product, pk=product_id)
 
     context = {
-        'product':product,
+        'product': product,
     }
+
     return render(request, 'products/product_detail.html', context)
+
 
 def add_product(request):
     """ Add a product to the store """
@@ -84,6 +86,30 @@ def add_product(request):
     template = 'products/add_product.html'
     context = {
         'form': form,
+    }
+
+    return render(request, template, context)
+
+
+def edit_product(request, product_id):
+    """ Edit a product in the store """
+    product = get_object_or_404(Product, pk=product_id)
+    if request.method == 'POST':
+        form = ProductForm(request.POST, request.FILES, instance=product)
+        if form.is_valid():
+            form.save()
+            messages.success(request, 'Successfully updated product!')
+            return redirect(reverse('product_detail', args=[product.id]))
+        else:
+            messages.error(request, 'Failed to update product. Please ensure the form is valid.')
+    else:
+        form = ProductForm(instance=product)
+        messages.info(request, f'You are editing {product.name}')
+
+    template = 'products/edit_product.html'
+    context = {
+        'form': form,
+        'product': product,
     }
 
     return render(request, template, context)

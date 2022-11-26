a6acdc2fec274669cf9a15c2bf3d8daf63a56015
martin-mcinerney <martin_mcinerney@live.ie>
2022-11-26 11:51:03 +0000

    Changed base dir setting

--- exclusive_whitening/settings.py
+++ exclusive_whitening/settings.py
@@ -14,8 +14,8 @@ from pathlib import Path
 import os 
 import dj_database_url
 
-# Build paths inside the project like this: BASE_DIR / 'subdir'.
-BASE_DIR = Path(__file__).resolve().parent.parent
+# Build paths inside the project like this: os.path.join(BASE_DIR, ...)
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 
 # Quick-start development settings - unsuitable for production
@@ -166,7 +166,7 @@ USE_TZ = True
 STATIC_URL = '/static/'
 STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
 
-MEDIA_URL = '/media/media'
+MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media',)
 
 if 'USE_AWS' in os.environ:

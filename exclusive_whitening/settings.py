81bb400acfe182cdf9cb06b9283fdf6ce2f841b5
martin-mcinerney <martin_mcinerney@live.ie>
2022-11-26 07:31:02 +0000

    Added secret key

--- exclusive_whitening/settings.py
+++ exclusive_whitening/settings.py
@@ -22,7 +22,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-6&_p(mry4by5&c^d=cic9791v3a5(kwtbjr3l$ptdm!wrf*9l6'
+SECRET_KEY = os.environ.get('SECRET_KEY', '')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True

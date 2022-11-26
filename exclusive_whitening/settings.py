ddd42c37ae4b1a3efcbe86636fcdc1d41860643b
martin-mcinerney <martin_mcinerney@live.ie>
2022-11-26 07:53:12 +0000

    Fixed secret key

--- exclusive_whitening/settings.py
+++ exclusive_whitening/settings.py
@@ -25,9 +25,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get('SECRET_KEY', '')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
-
-ALLOWED_HOSTS = ['localhost', 'https://ci-pp5.herokuapp.com/']
+DEBUG = 'DEVELOPMENT' in os.environ
+ALLOWED_HOSTS = ['localhost', 'https://ci-pp5.herokuapp.com/', '127.0.0.1']
 
 
 # Application definition

12bd2e6fd29188c78028d8ff3a79387b749d8cde
martin-mcinerney <martin_mcinerney@live.ie>
2022-11-26 07:21:07 +0000

    Added elephantsql and deployed heroku

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn exclusive_whitening.wsgi:app
--- exclusive_whitening/settings.py
+++ exclusive_whitening/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 
 from pathlib import Path
 import os 
+import dj_database_url
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -26,7 +27,7 @@ SECRET_KEY = 'django-insecure-6&_p(mry4by5&c^d=cic9791v3a5(kwtbjr3l$ptdm!wrf*9l6
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['localhost', 'https://ci-pp5.herokuapp.com/']
 
 
 # Application definition
@@ -116,13 +117,17 @@ WSGI_APPLICATION = 'exclusive_whitening.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+if 'DATABASE_URL' in os.environ:
+    DATABASES = {
+        'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
     }
-}
-
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        }
+    }
 
 # Password validation
 # https://docs.djangoproject.com/en/4.1/ref/settings/#auth-password-validators

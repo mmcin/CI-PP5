49e268440c62187b7b47ce3e05e9656e97da796b
martin-mcinerney <martin_mcinerney@live.ie>
2022-11-26 08:06:25 +0000

    Adds allowed urls

--- exclusive_whitening/settings.py
+++ exclusive_whitening/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY', '')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = 'DEVELOPMENT' in os.environ
-ALLOWED_HOSTS = ['localhost', 'https://ci-pp5.herokuapp.com/', '127.0.0.1']
+ALLOWED_HOSTS = ['localhost', 'ci-pp5.herokuapp.com/', 'https://martin-mcinerney-ci-pp5.herokuapp.com/', '127.0.0.1', ]
 
 
 # Application definition

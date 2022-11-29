3c68156d6bb4ba8713640569270d840e29bbefb5
martin-mcinerney <martin_mcinerney@live.ie>
2022-11-29 18:05:42 +0000

    adds blog.html and view

---
+++ blog/urls.py
@@ -0,0 +1,8 @@
+from django.conf import settings
+from django.contrib import admin
+from blog.views import blog
+from django.urls import path
+# post, about, search, postlist, allposts
+
+
+
--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
+from .models import Post, Category, Author
 
-# Create your views here.
+
+def blog(request):
+    categories = Category.objects.all()[0:3]
+    featured = Post.objects.filter(featured=True)
+    latest = Post.objects.order_by('-timestamp')[0:3]
+    context= {
+        'object_list': featured,
+        'latest': latest,
+        'categories':categories,
+    }
+    return render(request, 'blog.html', context)

c0c0a97a96f7ea4d1c1e841cd258c96f07d6f067
martin-mcinerney <martin_mcinerney@live.ie>
2022-11-29 18:22:31 +0000

    Fixed URLS in blog

--- blog/urls.py
+++ blog/urls.py
@@ -1,15 +1,12 @@
 from django.conf import settings
 from django.contrib import admin
-from blog.views import blog
 from django.urls import path
-# post, about, search, postlist, allposts
+from blog.views import blog, post, category_post_list, allposts
 
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
     path('', blog, name = 'blog'),
     path('post/<slug>/', post, name = 'post'),
-    path('about/', about,name = 'about' ),
-    path('postlist/<slug>/', postlist, name = 'postlist'),
+    path('postlist/<slug>/', category_post_list, name = 'postlist'),
     path('posts/', allposts, name = 'allposts'),
 ]

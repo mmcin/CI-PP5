ae52fbce1d290fba6de7369e15b680fceb4ab6fb
martin-mcinerney <martin_mcinerney@live.ie>
2022-11-30 08:37:42 +0000

    Removed unused urls and templates

--- blog/urls.py
+++ blog/urls.py
@@ -5,8 +5,6 @@ from blog.views import blog, post, category_post_list, allposts
 
 
 urlpatterns = [
-    path('', blog, name = 'blog'),
     path('post/<slug>/', post, name = 'post'),
-    path('postlist/<slug>/', category_post_list, name = 'postlist'),
     path('posts/', allposts, name = 'allposts'),
 ]

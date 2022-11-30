7d085ad4552e457f506c793b86f8e4356abe8493
martin-mcinerney <martin_mcinerney@live.ie>
2022-11-30 09:10:48 +0000

    Adds product review models and admin

--- products/admin.py
+++ products/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Product, Category
+from .models import Product, Category, ProductReview
 # Register your models here.
 
 
@@ -24,4 +24,7 @@ class CategoryAdmin(admin.ModelAdmin):
 admin.site.register(Product, ProductAdmin)
 
 admin.site.register(Category, CategoryAdmin)
+
+admin.site.register(ProductReview)
+
 
--- products/models.py
+++ products/models.py
@@ -1,5 +1,5 @@
 from django.db import models
-
+from django.contrib.auth.models import User
 # Create your models here.
 
 class Category(models.Model):
@@ -28,3 +28,14 @@ class Product(models.Model):
 
     def __str__(self):
         return self.name
+
+class ProductReview(models.Model):
+    product = models.ForeignKey(Product, related_name ='reviews', on_delete=models.CASCADE)
+    user = models.ForeignKey(User, related_name ='reviews', on_delete=models.CASCADE)
+    content = models.TextField(blank = True, null = True)
+    title = models.TextField()
+
+
+    def __str__(self):
+        return self.title
+    

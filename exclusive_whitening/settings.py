912ff024f049abedfe1e3a2874a229eab698b53b
martin-mcinerney <martin_mcinerney@live.ie>
2022-12-01 11:37:18 +0000

    Whitenoise setup

--- exclusive_whitening/settings.py
+++ exclusive_whitening/settings.py
@@ -168,7 +168,7 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 # STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
-STATIC_ROOT = BASE_DIR / "static"
+STATIC_ROOT = 'static'
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')

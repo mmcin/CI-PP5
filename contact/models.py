debf5e05fb47050e4149b58f528af251f93b9e3b
martin-mcinerney <martin_mcinerney@live.ie>
2022-12-02 09:28:19 +0000

    Edited contact model

--- contact/models.py
+++ contact/models.py
@@ -4,9 +4,9 @@ from django.db import models
 class Message(models.Model):
     name = models.CharField(max_length = 254)
     phone_number = models.IntegerField()
-    email = models.CharField(max_length = 254, )
+    email = models.EmailField(max_length = 254)
     subject = models.CharField(max_length = 254)
-    message = models.CharField(max_length = 1000)
+    message = models.TextField(max_length = 1000)
 
     def __str__(self):
         return self.name

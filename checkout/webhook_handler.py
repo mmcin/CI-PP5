197b69d5ec12f3b2ee4f32d9c53654800400e4ab
martin-mcinerney <martin_mcinerney@live.ie>
2022-12-08 10:03:06 +0000

    Adds http response test pi succeed wh

--- checkout/webhook_handler.py
+++ checkout/webhook_handler.py
@@ -46,19 +46,17 @@ class StripeWH_Handler:
         """
         Handle the payment_intent.succeeded webhook from Stripe
         """
+        return HttpResponse(
+            content=f'Webhook that I want to check received: {event["type"]}',
+            status=200)
         intent = event.data.object
         pid = intent.id
         bag = intent.metadata.bag
         save_info = intent.metadata.save_info
 
-        # Get the Charge object
-        stripe_charge = stripe.Charge.retrieve(
-            intent.latest_charge
-        )
-
-        billing_details = stripe_charge.billing_details # updated
+        billing_details = intent.charges.data[0].billing_details
         shipping_details = intent.shipping
-        grand_total = round(stripe_charge.amount / 100, 2) # updated
+        grand_total = round(intent.charges.data[0].amount / 100, 2)
 
         # Clean data in the shipping details
         for field, value in shipping_details.address.items():

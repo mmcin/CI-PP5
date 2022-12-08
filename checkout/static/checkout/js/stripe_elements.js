0328589151fd723e121328945c8519b308cf8309
martin-mcinerney <martin_mcinerney@live.ie>
2022-12-08 16:28:40 +0000

    Adds form.submit to stripeelements js

--- checkout/static/checkout/js/stripe_elements.js
+++ checkout/static/checkout/js/stripe_elements.js
@@ -114,7 +114,7 @@ form.addEventListener('submit', function (ev) {
                 $('#submit-button').attr('disabled', false);
             } else {
                 if (result.paymentIntent.status === 'succeeded') {
-                    // form.submit();
+                    form.submit();
                 }
             }
         });
